01d47ed708216369295c459211ba449df6a52b8e
hank <hank+shannon@shannonsupply.com>
2025-09-16 10:07:18 -0400

    Add comprehensive campaign tracking system and Mac launcher
    - Added campaign tracking modal with 4 tabs (Overview, Battle History, Add Battle, Sharkanoid Stats)
    - Implemented battle recording with participant tracking, casualties, and experience gain
    - Added injury tracking for campaign battles
    - Experience and victory tracking for individual Sharkanoids
    - Campaign data persistence using localStorage
    - Comprehensive campaign statistics and battle history
    - Enhanced CSS styling for campaign modal with responsive design
    - Updated type definitions for campaign and battle objects
    - Created Mac launcher script (Launch Planet Shark.command) for easy app access
    - Modal includes tabs for managing campaign progression and tracking Sharkanoid performance over time

--- shiver-builder-app/js/types.js
+++ shiver-builder-app/js/types.js
@@ -4,6 +4,34 @@
  */
 
 /**
+ * @typedef {Object} CampaignBattle
+ * @property {string} id - Unique battle identifier
+ * @property {string} name - Battle name
+ * @property {string} date - Battle date
+ * @property {string} opponent - Opponent shiver name
+ * @property {string} result - Battle result (victory/defeat/draw)
+ * @property {string} scenario - Scenario played
+ * @property {Array<string>} casualties - Sharkanoid IDs that were casualties
+ * @property {Array<string>} survivors - Sharkanoid IDs that survived
+ * @property {number} experienceGained - Experience points gained
+ * @property {string} notes - Battle notes
+ */
+
+/**
+ * @typedef {Object} Campaign
+ * @property {string} id - Unique campaign identifier
+ * @property {string} name - Campaign name
+ * @property {string} description - Campaign description
+ * @property {Array<CampaignBattle>} battles - List of battles
+ * @property {number} totalExperience - Total experience gained
+ * @property {number} victories - Number of victories
+ * @property {number} defeats - Number of defeats
+ * @property {number} draws - Number of draws
+ * @property {string} dateCreated - Campaign creation date
+ * @property {string} lastPlayed - Last battle date
+ */
+
+/**
  * @typedef {Object} Sharkanoid
  * @property {number} id - Unique identifier
  * @property {string} name - Name of the sharkanoid
@@ -16,6 +44,10 @@
  * @property {Array<SharkanoidInjury>} injuries - Array of injuries
  * @property {boolean} isLeader - Whether this is the leader
  * @property {number} usedSupply - Amount of supply used
+ * @property {number} experience - Campaign experience points
+ * @property {number} battlesParticipated - Number of battles participated in
+ * @property {number} battlesWon - Number of battles won
+ * @property {boolean} isVeteran - Whether promoted to veteran status
  * @property {number} health - Health stat
  * @property {number} speed - Speed stat
  * @property {number} armor - Armor stat
@@ -36,5 +68,3 @@
 
 /**
  * This JSDoc file provides type definitions for the Planet Shark Shiver Builder app.
- * Include this at the top of your app.js file.
- */
